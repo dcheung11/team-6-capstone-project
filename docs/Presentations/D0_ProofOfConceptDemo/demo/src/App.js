c7e245aa7f4b08dda0a073e0ec55b191d25751fa
dcheung11 <cheund11@mcmaster.ca>
2024-11-06 17:33:31 -0500

    Creating 2 views

--- docs/Presentations/D0_ProofOfConceptDemo/demo/src/App.js
+++ docs/Presentations/D0_ProofOfConceptDemo/demo/src/App.js
@@ -1,10 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import TeamHomePage from './containers/TeamHomePage';
+import CalendarPage from './containers/CalendarPage';
+
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">hello friends</header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<CalendarPage />} />
+        <Route path="/team" element={<TeamHomePage />} />
+      </Routes>
+    </Router>
   );
 }
 
