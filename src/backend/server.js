8aca9db715d48533b212ea45357c20cfc8737a67
dcheung11 <cheund11@mcmaster.ca>
2025-01-07 20:37:40 -0500

    Initial connect to MongoDB cluster, signup, getPlayers.

---
+++ src/backend/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const playersRoutes = require('./routes/players-routes');
+const HttpError = require('./models/http-error');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/api/players', playersRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError('Could not find this route.', 404);
+  throw error;
+});
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || 'An unknown error occurred!' });
+});
+
+mongoose
+  .connect('mongodb+srv://root:H2pAgdPcmUUw97se@capstone-cluster.ghx0o.mongodb.net/')
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
