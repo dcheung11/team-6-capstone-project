6464b42004ecaece1e0089e0846d939f42d0030c
dcheung11 <cheund11@mcmaster.ca>
2025-01-22 01:06:37 -0500

    Initial React App setup + initial NavBar.js

---
+++ src/frontend/src/App.js
@@ -0,0 +1,16 @@
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <h3>
+        GSA Rev 0 : Create /containers, implement Home.js, Announcements.js,
+        MyTeam.js, Schedule.js, Standings.js. Add NavBar to these components
+        when required
+      </h3>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/frontend/src/components/NavBar.js
@@ -0,0 +1,60 @@
+import React from "react";
+import {
+  AppBar,
+  Button,
+  Toolbar,
+  Box,
+  IconButton,
+  Container,
+} from "@mui/material";
+import { Person as PersonIcon } from "@mui/icons-material";
+
+const NavBar = () => {
+  return (
+    <AppBar
+      position="static"
+      style={{ backgroundColor: "#7A003C", height: 92 }}
+    >
+      <Container style={{ marginTop: "14px" }}>
+        <Toolbar disableGutters sx={{ height: 64 }}>
+          <Box sx={{ display: "flex", alignItems: "center", gap: 4 }}>
+            <img
+              src="/images/gsa.png"
+              alt="GSA Logo"
+              style={{
+                marginRight: "32px",
+                width: "50px",
+                height: "50px",
+              }}
+            />
+          </Box>
+
+          <Box sx={{ display: "flex", gap: 3 }}>
+            <Button color="inherit" href="/">
+              Home
+            </Button>
+            <Button color="inherit" href="/announcements">
+              Announcements
+            </Button>
+            <Button color="inherit" href="/my-team">
+              My Team
+            </Button>
+            <Button color="inherit" href="/schedule">
+              Schedule
+            </Button>
+            <Button color="inherit" href="/standings">
+              Standings
+            </Button>
+          </Box>
+          <Box sx={{ ml: "auto" }}>
+            <IconButton color="inherit" size="large">
+              <PersonIcon />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+
+export default NavBar;
