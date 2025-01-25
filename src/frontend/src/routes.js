6efe65e152ef6c13569042aa32d696e6c45d44cb
Jad Haytaoglu <jadhaytaoglu@hotmail.com>
2025-01-25 17:08:15 -0500

    Announcement Page outline done

--- src/frontend/src/routes.js
+++ src/frontend/src/routes.js
@@ -1,9 +1,12 @@
+import { Announcement } from '@mui/icons-material';
 import HomePage from './views/HomePage';
 import LoginPage from './views/LoginPage';
+import AnnouncementsPage from './views/AnnouncementsPage';
 
 const routes = [
+  { path: '/', component: LoginPage }, 
   { path: '/home', component: HomePage },
-  { path: '/', component: LoginPage },
+  { path: '/announcements', component: AnnouncementsPage },
 ];
 
 export default routes;
