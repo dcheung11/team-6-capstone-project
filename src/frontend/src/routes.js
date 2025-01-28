546e01a2d7c68070e6918d34db232be710a084bc
emmawigglesworth <emmawigz@gmail.com>
2025-01-28 13:56:12 -0500

    baseline pages

--- src/frontend/src/routes.js
+++ src/frontend/src/routes.js
@@ -4,6 +4,9 @@ import LeagueManagementPage from "./views/LeagueManagementPage";
 import LoginPage from "./views/LoginPage";
 import MyTeamPage from "./views/MyTeamPage";
 import AnnouncementsPage from './views/AnnouncementsPage';
+import CommissionerAnnouncementsPage from './views/CommissionerAnnouncementsPage';
+import CreateAnnouncementsPage from './views/CreateAnnouncementsPage';
+import EditAnnouncementsPage from './views/EditAnnouncementsPage';
 
 const routes = [
   { path: "/home", component: HomePage },
@@ -12,6 +15,9 @@ const routes = [
   { path: "/standings", component: StandingsPage },
   { path: "/manage", component: LeagueManagementPage, private: true },
   { path: '/announcements', component: AnnouncementsPage },
+  { path: '/manage/announcements', component: CommissionerAnnouncementsPage },// private: true },
+  { path: '/manage/announcements/edit/:id', component: EditAnnouncementsPage },// private: true },
+  { path: '/manage/announcements/create', component: CreateAnnouncementsPage },// private: true },
 ];
 
 export default routes;
