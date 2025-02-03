329c1fd2a8a6f276deaec9682ed165a145864b34
Derek Li <derekli33@hotmail.com>
2025-02-03 14:42:28 -0500

    fix local storage

--- src/frontend/src/hooks/AuthProvider.js
+++ src/frontend/src/hooks/AuthProvider.js
@@ -6,9 +6,7 @@ const REACT_APP_API_BASE_URL = "http://localhost:3001/api"; // replace with your
 
 const AuthProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [playerId, setPlayerId] = useState(
-    localStorage.getItem("playerId") || null
-  );
+  const [playerId, setPlayerId] = useState(localStorage.getItem("playerId") || null);
   const [token, setToken] = useState(localStorage.getItem("site") || "");
   const navigate = useNavigate();
   const login = async (email, password) => {
@@ -27,6 +25,8 @@ const AuthProvider = ({ children }) => {
         setToken(res.token);
         setIsLoggedIn(true);
         localStorage.setItem("site", res.token);
+        localStorage.setItem("playerId", res.playerId);
+        localStorage.setItem("email", res.email);
         navigate("/home");
         return;
       }
@@ -42,16 +42,12 @@ const AuthProvider = ({ children }) => {
     setPlayerId(null);
     setToken("");
     localStorage.removeItem("site");
+    localStorage.removeItem("playerId");
+    localStorage.removeItem("email");
     navigate("/");
   };
 
-  return (
-    <AuthContext.Provider
-      value={{ isLoggedIn, token, playerId, login, logOut }}
-    >
-      {children}
-    </AuthContext.Provider>
-  );
+  return <AuthContext.Provider value={{ isLoggedIn, token, playerId, login, logOut }}>{children}</AuthContext.Provider>;
 };
 
 export default AuthProvider;
