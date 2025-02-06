d54b9043d7c8ee75a4eb8a4211f18a88aec63b0d
dcheung11 <cheund11@mcmaster.ca>
2025-02-06 10:47:29 -0500

    Seasons API

--- src/frontend/src/api/season.js
+++ src/frontend/src/api/season.js
@@ -7,7 +7,7 @@ export async function createSeason(
   allowedDivisions = 4
 ) {
   try {
-    const response = await fetch(`${REACT_APP_API_BASE_URL}/season/create`, {
+    const response = await fetch(`${REACT_APP_API_BASE_URL}/seasons/create`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -30,7 +30,7 @@ export async function createSeason(
 
 export async function getOngoingSeasons() {
   try {
-    const response = await fetch(`${REACT_APP_API_BASE_URL}/season/ongoing`, {
+    const response = await fetch(`${REACT_APP_API_BASE_URL}/seasons/ongoing`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -51,7 +51,7 @@ export async function getOngoingSeasons() {
 
 export async function getUpcomingSeasons() {
   try {
-    const response = await fetch(`${REACT_APP_API_BASE_URL}/season/upcoming`, {
+    const response = await fetch(`${REACT_APP_API_BASE_URL}/seasons/upcoming`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -72,7 +72,7 @@ export async function getUpcomingSeasons() {
 
 export async function getArchivedSeasons() {
   try {
-    const response = await fetch(`${REACT_APP_API_BASE_URL}/season/archived`, {
+    const response = await fetch(`${REACT_APP_API_BASE_URL}/seasons/archived`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -84,6 +84,70 @@ export async function getArchivedSeasons() {
     if (!response.ok) {
       const error = await response.json();
       throw new Error(error.message || "Get archived seasons failed");
+    }
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function getAllSeasons() {
+  try {
+    const response = await fetch(`${REACT_APP_API_BASE_URL}/seasons`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+    console.log("Get All Seasons response:", data);
+    if (!response.ok) {
+      const error = await response.json();
+      throw new Error(error.message || "Get all seasons failed");
+    }
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function deleteSeason(id) {
+  try {
+    const response = await fetch(`${REACT_APP_API_BASE_URL}/seasons/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    console.log("Delete Season response:", response);
+    if (!response.ok) {
+      const error = await response.json();
+      throw new Error(error.message || "Delete season failed");
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function getSeasonById(id) {
+  try {
+    const response = await fetch(`${REACT_APP_API_BASE_URL}/seasons/${id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+    console.log("Get Season by ID response:", data);
+    if (!response.ok) {
+      const error = await response.json();
+      throw new Error(error.message || "Get season by ID failed");
     }
     return data;
   } catch (error) {
