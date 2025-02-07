ac95d0b729b42ff8c98b0668cd3157eeef01cecc
dcheung11 <cheund11@mcmaster.ca>
2025-02-07 12:26:14 -0500

    allow for multiple id lookup

--- src/backend/controllers/divisions-controllers.js
+++ src/backend/controllers/divisions-controllers.js
@@ -48,30 +48,34 @@ const createDivision = async (req, res, next) => {
 
   res.status(201).json({ division: createdDivision });
 };
-
 const getDivisionById = async (req, res, next) => {
-  const divisionId = req.params.id;
+  const divisionIds = req.params.id.split(",");
 
-  let division;
+  let divisions;
   try {
-    division = await Division.findById(divisionId);
+    // Fetching multiple divisions if more than one ID is provided
+    divisions = await Division.find({ _id: { $in: divisionIds } });
   } catch (err) {
     const error = new HttpError(
-      "Fetching division failed, please try again later.",
+      "Fetching divisions failed, please try again later.",
       500
     );
     return next(error);
   }
 
-  if (!division) {
+  if (!divisions || divisions.length === 0) {
     const error = new HttpError(
-      "Could not find division for the provided id.",
+      "Could not find division(s) for the provided id(s).",
       404
     );
     return next(error);
   }
 
-  res.json({ division: division.toObject({ getters: true }) });
+  res.json({
+    divisions: divisions.map((division) =>
+      division.toObject({ getters: true })
+    ),
+  });
 };
 
 const updateDivisionTeams = async (req, res, next) => {
