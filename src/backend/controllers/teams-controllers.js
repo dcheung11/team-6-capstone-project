9a6963e946e6fc2e67180a3cd236f7fa2027f9cd
dcheung11 <cheund11@mcmaster.ca>
2025-02-15 22:06:30 -0500

    Fix getScheduleByTeamId

--- src/backend/controllers/teams-controllers.js
+++ src/backend/controllers/teams-controllers.js
@@ -14,7 +14,10 @@ const getTeams = async (req, res, next) => {
       .populate("captainId") // Populating captain
       .populate("seasonId"); // Po;
   } catch (err) {
-    const error = new HttpError("Fetching teams failed, please try again later.", 500);
+    const error = new HttpError(
+      "Fetching teams failed, please try again later.",
+      500
+    );
     return next(error);
   }
   res.json({
@@ -25,7 +28,9 @@ const getTeams = async (req, res, next) => {
 const registerTeam = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return next(new HttpError("Invalid inputs passed, please check your data.", 422));
+    return next(
+      new HttpError("Invalid inputs passed, please check your data.", 422)
+    );
   }
   const { name, divisionId, captainId, roster, seasonId } = req.body;
 
@@ -37,7 +42,10 @@ const registerTeam = async (req, res, next) => {
       seasonId: seasonId,
     });
   } catch (err) {
-    const error = new HttpError("1 Creating team failed, please try again.", 500);
+    const error = new HttpError(
+      "1 Creating team failed, please try again.",
+      500
+    );
     return next(error);
   }
 
@@ -65,7 +73,10 @@ const registerTeam = async (req, res, next) => {
     const division = await Division.findById(divisionId);
     division.teams.push(createdTeam);
   } catch (err) {
-    const error = new HttpError("2 Creating team failed, please try again.", 500);
+    const error = new HttpError(
+      "2 Creating team failed, please try again.",
+      500
+    );
     return next(error);
   }
 
@@ -94,12 +105,18 @@ const getTeamsById = async (req, res, next) => {
       .populate("captainId")
       .populate("seasonId");
   } catch (err) {
-    const error = new HttpError("Fetching teams failed, please try again later.", 500);
+    const error = new HttpError(
+      "Fetching teams failed, please try again later.",
+      500
+    );
     return next(error);
   }
 
   if (!teams || teams.length === 0) {
-    const error = new HttpError("Could not find team(s) for the provided id(s).", 404);
+    const error = new HttpError(
+      "Could not find team(s) for the provided id(s).",
+      404
+    );
     return next(error);
   }
 
@@ -115,12 +132,18 @@ const getScheduleGamesByTeamId = async (req, res, next) => {
   try {
     team = await Team.findById(teamId);
   } catch (err) {
-    const error = new HttpError("Fetching team failed, please try again later.", 500);
+    const error = new HttpError(
+      "Fetching team failed, please try again later.",
+      500
+    );
     return next(error);
   }
 
   if (!team) {
-    const error = new HttpError("Could not find team for the provided id.", 404);
+    const error = new HttpError(
+      "Could not find team for the provided id.",
+      404
+    );
     return next(error);
   }
 
@@ -138,20 +161,34 @@ const getScheduleGamesByTeamId = async (req, res, next) => {
       },
     });
   } catch (err) {
-    const error = new HttpError("Fetching schedule failed, please try again later.", 500);
+    const error = new HttpError(
+      "Fetching schedule failed, please try again later.",
+      500
+    );
     return next(error);
   }
 
   if (!schedule) {
-    const error = new HttpError("Could not find schedule for the provided team id.", 404);
+    const error = new HttpError(
+      "Could not find schedule for the provided team id.",
+      404
+    );
     return next(error);
   }
 
   // filter schedule to return only games for the team
-  schedule.schedule.games = schedule.schedule.games.filter((game) => game.team1._id.toString() === teamId || game.team2._id.toString === teamId);
+  const games =
+    (schedule.schedule &&
+      schedule.schedule.games &&
+      schedule.schedule.games.filter(
+        (game) =>
+          game.team1._id.toString() === teamId ||
+          game.team2._id.toString === teamId
+      )) ||
+    [];
 
   res.json({
-    games: schedule.schedule.games.map((game) => game.toObject({ getters: true })),
+    games: games.map((game) => game.toObject({ getters: true })),
   });
 };
 
