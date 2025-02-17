d62d5fd6f678e4a8943798181a71a5d2c289c451
Jad Haytaoglu <jadhaytaoglu@hotmail.com>
2025-02-17 17:57:12 -0500

    forgot routing and added a few more functions to controller

---
+++ src/backend/routes/reschedule-routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const rescheduleController = require('../controllers/reschedule-controller');
+
+const router = express.Router();
+
+// Route to get all reschedule requests
+router.get('/', rescheduleController.getAllRequests);
+
+// Route to get a specific reschedule request by ID
+router.get('/:id', rescheduleController.getRequestById);
+
+// Route to create a new reschedule request
+router.post('/', rescheduleController.createRequest);
+
+// Route to update an existing reschedule request by ID
+router.put('/:id', rescheduleController.updateRequest);
+
+// Route to delete a reschedule request by ID
+router.delete('/:id', rescheduleController.deleteRequest);
+
+module.exports = router;
