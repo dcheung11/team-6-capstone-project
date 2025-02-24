00bb5e340727e85088595240aa2656fea7f8540f
emmawigglesworth <emmawigz@gmail.com>
2025-02-24 00:53:08 -0500

    typo fix and removing shame clm

--- src/frontend/src/components/StandingsTable.jsx
+++ src/frontend/src/components/StandingsTable.jsx
@@ -10,8 +10,8 @@ import {
 } from "@mui/material";
 
 export default function StandingsTable({ standings }) {
-  const standingColumns = ["Rank", "Team", "PTS", "W", "D", "L", "RS", "RA", "Run Diff", "No Show Shame"];
-  const standingKeys = ["rank", "team.teamName", "p", "w", "d", "l", "rs", "ra", "differential", "noshows"];
+  const standingColumns = ["Rank", "Team", "PTS", "W", "D", "L", "RS", "RA", "Run Diff"];//, "No Show Shame"];
+  const standingKeys = ["rank", "team.teamName", "p", "w", "d", "l", "rs", "ra", "differential"];//, "noshows"];
 
   return (
     <TableContainer component={Paper} sx={{ mb: 4 }}>
