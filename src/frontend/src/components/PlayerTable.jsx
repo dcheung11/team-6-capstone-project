f3f961e73fc2c22f2df6c244236faeeb09556828
dcheung11 <cheund11@mcmaster.ca>
2025-03-26 21:42:53 -0400

    use allPlayers call for faster processing

--- src/frontend/src/components/PlayerTable.jsx
+++ src/frontend/src/components/PlayerTable.jsx
@@ -20,7 +20,7 @@ export default function PlayerTable(props) {
   const auth = useAuth();
   const [playerId, setPlayerId] = useState(auth.playerId);
   const [user, setUser] = useState(null);
-  const [playersWithTeams, setPlayersWithTeams] = useState([]);
+  const [playersWithTeams, setPlayersWithTeams] = useState(props.players);
   const [loading, setLoading] = useState(false);
   const [showOnlyNoTeam, setShowOnlyNoTeam] = useState(true);
   const [searchQuery, setSearchQuery] = useState(""); // State for search input
@@ -33,53 +33,15 @@ export default function PlayerTable(props) {
         setUser(data.player);
       } catch (err) {
         console.log("Failed to fetch player");
-      } 
+      }
     };
 
     fetchPlayerById(playerId);
   }, []);
 
-  // multiple team logic
-  // console.log(user?.team?.captainId?.id, user?.id)
-
-  // Fetch player details by player ID
-  const fetchPlayerById = async (pid) => {
-    let data;
-    try {
-      data = await getPlayerById(pid);
-    } catch (err) {
-      console.error("Error fetching player:", err);
-    }
-    return data;
-  };
-
-  // Fetch all player details
-  // useEffect(() => {
-    const fetchPlayersDetails = async () => {
-      try {
-        setLoading(true);
-        const enrichedPlayers = await Promise.all(
-          props.players.map(async (player) => {
-            const playerData = await fetchPlayerById(player.id); // Fetch player details
-            return { ...player, team: playerData.player?.team?.name || null }; // Enrich player data with team name
-          })
-        );
-        setPlayersWithTeams(enrichedPlayers); // Set enriched players data
-      }
-      catch (err) {
-        console.log("Failed to fetch players");
-      }
-      finally {
-        setLoading(false);
-      }
-    };
-
-  //   fetchPlayersDetails(); // Fetch all player details on component mount
-  // }, [props.players]);
-
   useEffect(() => {
-    fetchPlayersDetails(); // Fetch all player details on component mount
-  }, [props.players]);
+    setPlayersWithTeams(props.players);
+  }, [props.players]); // Update state when props.players changes
 
   //Function to handle inviting a player to a team
   const handleInvite = (invitee) => {
@@ -107,16 +69,17 @@ export default function PlayerTable(props) {
     setShowOnlyNoTeam((prev) => !prev);
   };
 
-    const filteredPlayers = (showOnlyNoTeam
+  const filteredPlayers = (
+    showOnlyNoTeam
       ? playersWithTeams.filter((player) => !player.team)
       : playersWithTeams
-    ).filter((player) => {
-      const fullName = `${player.firstName} ${player.lastName}`.toLowerCase();
-      const isNotCommissioner = player.role !== 'commissioner'; // Exclude commissioner role
-      return fullName.includes(searchQuery.toLowerCase()) && isNotCommissioner;;
-    });
+  ).filter((player) => {
+    const fullName = `${player.firstName} ${player.lastName}`.toLowerCase();
+    const isNotCommissioner = player.role !== "commissioner"; // Exclude commissioner role
+    return fullName.includes(searchQuery.toLowerCase()) && isNotCommissioner;
+  });
 
-    // Sort players alphabetically by first name, then last name
+  // Sort players alphabetically by first name, then last name
   const sortedPlayers = [...filteredPlayers].sort((a, b) => {
     const nameA = `${a.firstName} ${a.lastName}`.toLowerCase();
     const nameB = `${b.firstName} ${b.lastName}`.toLowerCase();
@@ -129,40 +92,52 @@ export default function PlayerTable(props) {
         <div className="spinner">Loading...</div>
       ) : (
         <div>
-          <Box sx={{ display: 'flex', gap: 2, mb: 2 }}>
-           <TextField
-            label="Search Players"
-            variant="outlined"
-            fullWidth
-            sx={{ mb: 2, flexBasis: '66%'}}
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-          />
+          <Box sx={{ display: "flex", gap: 2, mb: 2 }}>
+            <TextField
+              label="Search Players"
+              variant="outlined"
+              fullWidth
+              sx={{ mb: 2, flexBasis: "66%" }}
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
             <Button
-        variant="contained"
-        sx={{ marginBottom: 2, backgroundColor: "#7A003C", color: "white", flexBasis: '33%' }}
-        onClick={toggleFilter}
-      >
-        {showOnlyNoTeam ? "Show All Players" : "Show Players with No Team"}
-      </Button>
-      </Box>
-        
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell sx={{ fontWeight: 'bold' }}>Player</TableCell>
-              <TableCell sx={{ fontWeight: 'bold' }}>Team</TableCell>
-              <TableCell sx={{ fontWeight: 'bold' }}>Action</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {sortedPlayers.map((player, index) => (
-              <TableRow key={index}>
-                <TableCell>{player.firstName} {player.lastName}</TableCell>
-                <TableCell>{player.team ? player.team : "No Team"}</TableCell>
-                <TableCell>
-                  {!player.team ? (
-                    !player.invites?.includes(user.team._id) ? (
+              variant="contained"
+              sx={{
+                marginBottom: 2,
+                backgroundColor: "#7A003C",
+                color: "white",
+                flexBasis: "33%",
+              }}
+              onClick={toggleFilter}
+            >
+              {showOnlyNoTeam
+                ? "Show All Players"
+                : "Show Players with No Team"}
+            </Button>
+          </Box>
+
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell sx={{ fontWeight: "bold" }}>Player</TableCell>
+                <TableCell sx={{ fontWeight: "bold" }}>Team</TableCell>
+                <TableCell sx={{ fontWeight: "bold" }}>Action</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {sortedPlayers.map((player, index) => (
+                <TableRow key={index}>
+                  <TableCell>
+                    {player.firstName} {player.lastName}
+                  </TableCell>
+                  <TableCell>
+                    {player?.team ? player.team.name : "No Team"}
+                  </TableCell>
+                  <TableCell>
+                    {player?.team ? null : !player?.invites?.includes(
+                        user?.team?._id
+                      ) ? (
                       <Button
                         variant="contained"
                         sx={{
@@ -186,13 +161,12 @@ export default function PlayerTable(props) {
                       >
                         Invite Sent
                       </Button>
-                    )
-                  ) : null}
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+                    )}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
         </div>
       )}
     </TableContainer>
